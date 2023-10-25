75d0ea2fb166d5b874c62d27aaa9e35afd3a21a5
FedericaCampo97 <federica.campo97@gmail.com>
2023-10-25 22:16:54 +0200

    Fixed bug, exercise complete

--- resources/views/dogs.blade.php
+++ resources/views/dogs.blade.php
@@ -12,7 +12,7 @@
     <div>
         <ul class="nav nav-tabs">
             <li class="nav-item">
-                <a class="nav-link" aria-current="page" href="#">Home</a>
+                <a class="nav-link" aria-current="page" href="./">Home</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link active" href="{{ route('dogs') }}">Dogs</a>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
         <div>
             <ul class="nav nav-tabs">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    <a class="nav-link active" aria-current="page" href="./">Home</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="{{ route('dogs') }}">Dogs</a>
